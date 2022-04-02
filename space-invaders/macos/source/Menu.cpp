79e79cdc1600d79f0c15d93d8b12f372e06bb996
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-02 19:42:35 +0200

    Menu

--- space-invaders/macos/source/Menu.cpp
+++ space-invaders/macos/source/Menu.cpp
@@ -6,10 +6,17 @@
 //
 
 #include "Menu.hpp"
+#include "Color.hpp"
 #include "Common.hpp"
+#include <iostream>
 
 Menu::Menu(SDL_Renderer* renderer) {
     this->renderer = renderer;
+    isRunning = false;
+    currentTick = SDL_GetPerformanceCounter();
+    lastTick = 0;
+    deltaTime = 0.0;
+
     load();
     loop();
 }
@@ -18,17 +25,29 @@ Menu::~Menu() {
 }
 
 void Menu::load() {
-    font48 = TTF_OpenFont("data/font.ttf", 48);
+    font60 = TTF_OpenFont("data/font.ttf", 60);
+    font40 = TTF_OpenFont("data/font.ttf", 24);
+    
+    surfaceTitle = TTF_RenderText_Blended(font60, "Space Invaders", Color::getOrange());
+	title = SDL_CreateTextureFromSurface(renderer, surfaceTitle);
 
-    SDL_Surface* surfaceMessage = TTF_RenderText_Blended(font48, "Space Invaders", textColor);
-	SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+    surfaceStartWhite = TTF_RenderText_Blended(font40, "- Press Enter to Start -", Color::getWhite());
+    startWhiteTexture = SDL_CreateTextureFromSurface(renderer, surfaceStartWhite);
+
+    surfaceStartGray = TTF_RenderText_Blended(font40, "- Press Enter to Start -", Color::getGray());
+    startGrayTexture = SDL_CreateTextureFromSurface(renderer, surfaceStartGray);
 }
 
 void Menu::loop() {
-    for(short i = 1; i < 5; i++) {
+    isRunning = true;
+    while(isRunning) {
+        lastTick = currentTick;
+		currentTick = SDL_GetPerformanceCounter();
+		deltaTime = (double) ((currentTick - lastTick) * 1000 / (double) SDL_GetPerformanceFrequency());
+        
         input();
         render();
-        SDL_Delay(1000);
+        SDL_Delay(deltaTime);
     }
 }
 
@@ -36,7 +55,38 @@ void Menu::render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-   
+    SDL_Point size;
+    SDL_QueryTexture(title, NULL, NULL, &size.x, &size.y);
+    
+    int speed = 8;
+    float pi = 3.141592f;
+    int offset = 30;
+    
+    SDL_Rect rect = { 
+        Common::H_WIDTH - (size.x / 2), 
+        static_cast<int>(speed * SDL_sin(SDL_GetTicks() * (pi / 1500.0f))) + offset,
+        size.x,
+        size.y 
+    };
+    SDL_RenderCopy(renderer, title, NULL, &rect);
+
+    SDL_QueryTexture(startWhiteTexture, NULL, NULL, &size.x, &size.y);
+    SDL_Rect whiteRect = { 
+        Common::H_WIDTH - (size.x / 2), 
+        Common::H_HEIGHT + 100 - (size.y / 2),
+        size.x,
+        size.y 
+    };
+    SDL_RenderCopy(renderer, startWhiteTexture, NULL, &whiteRect);
+
+    SDL_QueryTexture(startGrayTexture, NULL, NULL, &size.x, &size.y);
+    SDL_Rect grayRect = { 
+        Common::H_WIDTH - (size.x / 2), 
+        Common::H_HEIGHT + 100 - (size.y / 2) + 3,
+        size.x,
+        size.y 
+    };
+    SDL_RenderCopy(renderer, startGrayTexture, NULL, &grayRect);
 
     SDL_RenderPresent(renderer);   
 }
@@ -46,17 +96,14 @@ void Menu::input() {
     while (SDL_PollEvent(&sdlEvent)) {
         switch (sdlEvent.type) {
             case SDL_QUIT:
+                isRunning = false;
                 break;
         }
     }
 }
 
 void Menu::unload() {
-    SDL_DestroyTexture(logo_texture);
-    SDL_FreeSurface(logo_surface);
+    SDL_DestroyTexture(title);
+    SDL_FreeSurface(surfaceTitle);
+    TTF_CloseFont(font60);
 }
-
-
-
-
-
