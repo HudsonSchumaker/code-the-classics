8a7f66b4bb08f7be310cde77073f877883442945
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-02 18:10:33 +0200

    Add Color.hpp

---
+++ space-invaders/macos/source/Color.hpp
@@ -0,0 +1,25 @@
+// 
+// macOS
+// Space Invaders
+// SchumakerTeam
+// Hudson Schumaker
+//
+
+#pragma once
+#include <SDL2/SDL.h>
+
+class Color final {
+    public:
+        Color();
+        ~Color();
+
+        static SDL_Color getRed();
+        static SDL_Color getGreen();
+        static SDL_Color getBlue();
+        static SDL_Color getWhite();
+        static SDL_Color getBlack();
+        static SDL_Color getGray();
+        static SDL_Color getOrange();
+        static SDL_Color getYellow();
+        static SDL_Color getPink();
+};
