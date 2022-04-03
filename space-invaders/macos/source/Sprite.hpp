d8c0d14e9c0e0a6d7ad528f6c2a30f34b9b5ac64
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-03 21:52:38 +0200

    Sprite

--- space-invaders/macos/source/Sprite.hpp
+++ space-invaders/macos/source/Sprite.hpp
@@ -9,7 +9,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
-#include "Rectangle.hpp"
+//#include "Rectangle.hpp"
 
 class Sprite {
 protected:
@@ -21,24 +21,19 @@ public:
 	Sprite();
 	Sprite(int x, int y);
     Sprite(int x, int y, int w, int h);
-	Rectangle getBounds();
+//	Rectangle getBounds();
 	virtual void draw(SDL_Renderer* renderer) = 0;
-	
-	void setTexture(SDL_Texture* spr) {
-		SDL_Point size;
-    	SDL_QueryTexture(spr, NULL, NULL, &size.x, &size.y);
-		sprite = spr; w = size.x; h = size.y;
-	};
-		
-	int getX() const { return x; };
-	void setX(int x) { this->x = x; };
+	void setTexture(SDL_Texture* spr);
+				
+	const int getX();
+	void setX(int x);
 
-    int getY() const { return y; };
-    void setY(int y) { this->y = y; };
+    int getY() const;
+    void setY(int y);
 
-    int getWidth() const { return w; };
-    void setWidth(int w) { this->w = w; };
+    int getWidth() const;
+    void setWidth(int w);
 
-	int getHeight() const { return h; };
-   	void setHeight(int h) { this->h = h; };
+	int getHeight() const;
+   	void setHeight(int h);
 };
