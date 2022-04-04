dabc8309b62a597f1b7ace8a645f7a5151d6df6d
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-04 20:07:48 +0200

    Sprite

--- space-invaders/macos/source/Collider2D.cpp
+++ space-invaders/macos/source/Collider2D.cpp
@@ -11,6 +11,19 @@ Collider2D::Collider2D() : x(0), y(0), w(0), h(0) {}
 Collider2D::Collider2D(int w, int h) : x(0), y(0), w(w), h(h) {} 
 Collider2D::Collider2D(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
 
+bool Collider2D::collide(const Collider2D& a, const Collider2D& b) {
+    int pwA = a.getX() + a.getWidth();
+    int pwB = b.getX() + b.getWidth();
+
+    int phA = a.getY() + a.getHeight();
+    int phB = b.getY() + b.getHeight();
+
+    if (pwA > b.getX() && a.getX() < pwB && phA > b.getY() && a.getY() < phB) {
+        return true;
+    }
+    return false;
+}
+
 void Collider2D::setX(int x) { this->x = x; };
 int Collider2D::getX() const { return x; };
 
