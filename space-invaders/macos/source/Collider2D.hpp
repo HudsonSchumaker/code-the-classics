6af3ddf97eecdf0280cbf98be8830ad2695fa754
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-04 20:05:54 +0200

    Sprite

--- space-invaders/macos/source/Collider2D.hpp
+++ space-invaders/macos/source/Collider2D.hpp
@@ -16,4 +16,17 @@ class Collider2D final {
         Collider2D();
         Collider2D(int w, int h);
         Collider2D(int x, int y, int w, int h);
+
+
+        void setX(int x);
+	    int getX() const;
+
+	    void setY(int y);
+	    int getY() const;
+
+	    void setWidth(int w);
+	    int getWidth() const;
+
+	    void setHeight(int h);
+	    int getHeight() const;
 };
