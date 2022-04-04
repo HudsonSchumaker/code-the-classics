e02de71902f668fa0f49c431177d15674c0eda55
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-04 20:22:09 +0200

    Player

--- space-invaders/macos/source/Player.cpp
+++ space-invaders/macos/source/Player.cpp
@@ -4,3 +4,25 @@
 // SchumakerTeam
 // Hudson Schumaker
 //
+
+void Player::moveX(int dx) {
+    this->x += dx;
+}
+
+void Player::moveY(int dy) {
+    this->y += dy;
+}
+
+void Player::move(int dx, int dy) {
+    this->x += dx;
+    this->y += dy;
+}
+
+void Player::setPos(int dx, int dy) {
+    this->x = dx;
+    this->y = dy;
+}
+
+void Player::draw() {
+    GRRLIB_DrawImg(x, y, sprite, 0, 1, 1, Color::getWhite());
+}
