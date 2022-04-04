dca974f5bd41b84f0f96c10ce5c060683cecaa17
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-04 20:18:34 +0200

    Player

---
+++ space-invaders/macos/source/Player.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Sprite.hpp"
+
+class Player final: public Sprite {
+    private:
+        bool destroyed;
+
+    public:
+        Player() : Sprite(0, 0), destroyed(false) {};
+        Player(int x, int y) : Sprite(x, y), destroyed(false) {};
+        Player(int x, int y, int w, int h) : Sprite(x, y, w, h), destroyed(false) {};
+        Player(int x, int y, int w, int h, bool d) : Sprite(x, y, w, h), destroyed(d) {};
+    
+        void moveX(int dx);
+        void moveY(int dy);
+        void move(int dx, int dy);
+        void draw(SDL_Renderer* renderer) override;
+        void setPos(int dx, int dy);
+
+        bool isDestroyed() const { return destroyed; };
+        void setDestroyed(bool d) { this->destroyed = d; };
+};
