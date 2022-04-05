ba2b4c85a0b3cf3d9366a4e9e35f1a16cac64fc3
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-05 22:09:32 +0200

    Invaders

--- space-invaders/macos/source/Invader.hpp
+++ space-invaders/macos/source/Invader.hpp
@@ -10,5 +10,21 @@
 #include "Sprite.hpp"
 
 class Invaders final : public Sprite {
+    
+    private:
+        bool destroyed;
+        int speed;
+
+    public:
+        Invaders(int x, int y, int w, int h) : Sprite(x, y, w, h), destroyed(false) {};
+        void moveX(int dx);
+        void moveY(int dy);
+        void move(int dx, int dy);
+        void draw(SDL_Renderer* renderer) override;
+        void setPos(int px, int py);
+        void setSpeed(int s);
+
+        bool isDestroyed() const;
+        void setDestroyed(bool d);
 
 };
