c43e617739cc6447fca91aa265ed89b5f40a48f7
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-05 20:50:20 +0200

    Common changes

--- space-invaders/macos/source/Common.hpp
+++ space-invaders/macos/source/Common.hpp
@@ -10,9 +10,12 @@
 class Common final {
     public:
         inline static const char TITLE[] = "Space Invaders";
-        static const int WIDTH = 680;
+        
+        static const int FPS = 144;
+        static const int MILLISECS_PER_FRAME = 1000 / FPS;
+        static const int WIDTH = 640;
         static const int H_WIDTH = WIDTH / 2;
 
-        static const int HEIGHT = 540;
+        static const int HEIGHT = 480;
         static const int H_HEIGHT = HEIGHT / 2;
 };
