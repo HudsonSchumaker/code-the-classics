5e2313eee68f60225c58d5ac551d607b249eb4c1
HudsonSchumaker <hudson.schumaker@me.com>
2022-04-06 13:45:17 +0200

    Metal is back

--- space-invaders/macos/source/Main.cpp
+++ space-invaders/macos/source/Main.cpp
@@ -23,7 +23,7 @@ void setUp() {
         Common::TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         Common::WIDTH,
         Common::HEIGHT,
-        SDL_WINDOW_SHOWN
+        SDL_WINDOW_SHOWN | SDL_WINDOW_METAL
     );
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);   
